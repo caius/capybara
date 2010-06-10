86079319b908219380c7fcae8f56b0d8b0996a32
Caius Durling <dev@caius.name>
2010-06-10 21:23:37 +0100

    Adding specs for Capybara#culerity_driver_spec and Capybara::Driver::Culerity#browser_options

--- spec/capybara_spec.rb
+++ spec/capybara_spec.rb
@@ -14,5 +14,21 @@ describe Capybara do
       Capybara.default_wait_time.should == 5
     end
   end
-  
+
+  describe "#culerity_driver_options" do
+    before do
+      # Reset it back to defaults
+      Capybara.culerity_driver_options = nil
+    end
+
+    it "should default to an empty hash" do
+      Capybara.culerity_driver_options.should == {}
+    end
+
+    it "should take a custom value" do
+      Capybara.culerity_driver_options = {:foo => :bar}
+      Capybara.culerity_driver_options.should == {:foo => :bar}
+    end
+  end
+
 end
--- spec/driver/culerity_driver_spec.rb
+++ spec/driver/culerity_driver_spec.rb
@@ -8,5 +8,26 @@ describe Capybara::Driver::Culerity do
   it_should_behave_like "driver"
   it_should_behave_like "driver with javascript support"
   it_should_behave_like "driver with header support"
-  
+
+  describe "#browser_options" do
+    before do
+      # Make sure we're using the defaults
+      Capybara.culerity_driver_options = nil
+    end
+
+    it "should default to sane defaults" do
+      @driver.send(:browser_options).should == {:browser => :firefox, :log_level => :off}
+    end
+
+    it "should use any additional settings" do
+      Capybara.culerity_driver_options = {:foo => :bar}
+      @driver.send(:browser_options).should == {:browser => :firefox, :log_level => :off, :foo => :bar}
+    end
+
+    it "should override defaults with custom setting" do
+      Capybara.culerity_driver_options = {:browser => :internet_explorer}
+      @driver.send(:browser_options).should == {:browser => :internet_explorer, :log_level => :off}
+    end
+  end
+
 end
